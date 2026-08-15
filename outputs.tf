024acfac3b64c8998b473bb7d9b9fad5f6e940dc
Daniel Allen <dan@aeternagroup.net>
2026-08-15 02:17:19 +0100

    Update to schema version v6.58.0

--- outputs.tf
+++ outputs.tf
@@ -4,11 +4,11 @@ output "sesv2_account_vdm_attributes_id" {
 }
 output "sesv2_account_vdm_attributes_dashboard_attributes" {
   description = "Map of dashboard_attributes values across all sesv2_account_vdm_attributes, keyed the same as var.sesv2_account_vdm_attributes"
-  value       = { for k, v in aws_sesv2_account_vdm_attributes.sesv2_account_vdm_attributes : k => v.dashboard_attributes if v.dashboard_attributes != null && length(v.dashboard_attributes) > 0 }
+  value       = { for k, v in aws_sesv2_account_vdm_attributes.sesv2_account_vdm_attributes : k => one(v.dashboard_attributes) if v.dashboard_attributes != null && length(v.dashboard_attributes) > 0 }
 }
 output "sesv2_account_vdm_attributes_guardian_attributes" {
   description = "Map of guardian_attributes values across all sesv2_account_vdm_attributes, keyed the same as var.sesv2_account_vdm_attributes"
-  value       = { for k, v in aws_sesv2_account_vdm_attributes.sesv2_account_vdm_attributes : k => v.guardian_attributes if v.guardian_attributes != null && length(v.guardian_attributes) > 0 }
+  value       = { for k, v in aws_sesv2_account_vdm_attributes.sesv2_account_vdm_attributes : k => one(v.guardian_attributes) if v.guardian_attributes != null && length(v.guardian_attributes) > 0 }
 }
 output "sesv2_account_vdm_attributes_region" {
   description = "Map of region values across all sesv2_account_vdm_attributes, keyed the same as var.sesv2_account_vdm_attributes"
